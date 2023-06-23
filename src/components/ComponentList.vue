3b846fb1060d1a9591d2a3c58a7219f37b6c1d4b
Cosimo-Colucci <ccoluccilavoro@gmail.com>
2023-06-23 13:01:20 +0200

    get img of country by methods

--- src/components/ComponentList.vue
+++ src/components/ComponentList.vue
@@ -4,31 +4,49 @@
         name: 'ComponentList',
         data() {
             return {
+                availablelenguages :[
+                    'it.png', 'en.png', 'fr.png',
+                ]
             };
         },
         props:{
             componentList : Array,
         },
         methods:{
+            isLanguageFlagPresent(language){
+                if(this.availablelenguages.includes(language)){
+                    return true
+                }
+                return false;
+            },
+            getImagePath(img){
+                return new URL (`../assets/img/${img}.png`, import.meta.url).href;
+            }
 
         },
     }
 </script>
 
 <template>
+    
     <div>
-        <h1>
-            porca
-        </h1>
-    </div>
-    <div>
-        <article v-for="movie in movies">
+        <article v-for="movie in componentList">
             <h1>
                 {{ movie.title }}
             </h1>
             <h2>
                 {{ movie.original_title }}
             </h2>
+
+            <img v-if="isLanguageFlagPresent(movie.original_language)" 
+            :src="getImagePath(movie.original_language)" alt="">
+            <pre v-else>
+                {{ movie.original_language }}
+            </pre>
+
+            <span>
+                {{ movie.vote_average }}
+            </span>
         </article>
     </div>
 </template>
